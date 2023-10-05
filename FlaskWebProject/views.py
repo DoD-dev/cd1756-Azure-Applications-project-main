9af9252596a37156feeb35198850f1edf015a9c0
DoD-dev <cautapchung98@gmail.com>
2023-10-05 16:04:40 +0700

    change INFO -> info

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -66,7 +66,7 @@ def login():
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
-            app.logger.INFO('Invalid login attempt')
+            app.logger.info('Invalid login attempt')
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
@@ -83,7 +83,7 @@ def authorized():
     if request.args.get('state') != session.get("state"):
         return redirect(url_for("home"))  # No-OP. Goes back to Index page
     if "error" in request.args:  # Authentication/Authorization failure
-        app.logger.INFO('Invalid login attempt')
+        app.logger.info('Invalid login attempt')
         return render_template("auth_error.html", result=request.args)
     if request.args.get('code'):
         cache = _load_cache()
@@ -93,7 +93,7 @@ def authorized():
             scopes=Config.SCOPE,
             redirect_uri=url_for('authorized', _external=True, _scheme='https'))
         if "error" in result:
-            app.logger.INFO('Invalid login attempt')
+            app.logger.info('Invalid login attempt')
             return render_template("auth_error.html", result=result)
         session["user"] = result.get("id_token_claims")
         # Note: In a real app, we'd use the 'name' property from session["user"] below
@@ -101,7 +101,7 @@ def authorized():
         user = User.query.filter_by(username="admin").first()
         login_user(user)
         _save_cache(cache)
-        app.logger.INFO('admin logged in successfully')
+        app.logger.info('admin logged in successfully')
     return redirect(url_for('home'))
 
 @app.route('/logout')
