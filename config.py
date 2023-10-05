87e1b8556181b170609327ca9dfd58f7ec09f285
DoD-dev <cautapchung98@gmail.com>
2023-10-05 15:37:28 +0700

    update new config and add log

--- config.py
+++ config.py
@@ -5,12 +5,12 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret-key'
 
-    BLOB_ACCOUNT = os.environ.get('BLOB_ACCOUNT') or 'dodarticlecmsmages'
-    BLOB_STORAGE_KEY = os.environ.get('BLOB_STORAGE_KEY') or 'XUZ7wldJWKlTKWdfarjQSyZzWpZqVsrSihno0LjMRfr9i9u1xBvqKWTTc0nd8ykuWJ/ChB8+T/ed+ASt63WQfQ=='
-    BLOB_CONTAINER = os.environ.get('BLOB_CONTAINER') or 'dodarticlecmsblob'
+    BLOB_ACCOUNT = os.environ.get('BLOB_ACCOUNT') or 'dodarticlecms'
+    BLOB_STORAGE_KEY = os.environ.get('BLOB_STORAGE_KEY') or '681sQ/AJgAe2WVhrrc0W7pxWJTV2oj4ncniVTD2Xb4u2A+h6gbXYlfTbGEHrjQzzdo5fELqnWDgo+AStzt03VQ=='
+    BLOB_CONTAINER = os.environ.get('BLOB_CONTAINER') or 'dod-article-cms-blob'
 
-    SQL_SERVER = os.environ.get('SQL_SERVER') or 'dod-udacity-database-server.database.windows.net'
-    SQL_DATABASE = os.environ.get('SQL_DATABASE') or 'dod-article-cms-udacity'
+    SQL_SERVER = os.environ.get('SQL_SERVER') or 'dod-article-cms-dbserver.database.windows.net'
+    SQL_DATABASE = os.environ.get('SQL_DATABASE') or 'dod-article-cms-db'
     SQL_USER_NAME = os.environ.get('SQL_USER_NAME') or 'dod'
     SQL_PASSWORD = os.environ.get('SQL_PASSWORD') or 'abcde12345-'
     # Below URI may need some adjustments for driver version, based on your OS, if running locally
@@ -19,7 +19,7 @@ class Config(object):
 
     ### Info for MS Authentication ###
     ### As adapted from: https://github.com/Azure-Samples/ms-identity-python-webapp ###
-    CLIENT_SECRET = "ENTER_CLIENT_SECRET_HERE"
+    CLIENT_SECRET = "~6C8Q~AvV.wULgW21bp4hspyjFAN0oUvFukkgbnG"
     # In your production app, Microsoft recommends you to use other ways to store your secret,
     # such as KeyVault, or environment variable as described in Flask's documentation here:
     # https://flask.palletsprojects.com/en/1.1.x/config/#configuring-from-environment-variables
@@ -27,10 +27,10 @@ class Config(object):
     # if not CLIENT_SECRET:
     #     raise ValueError("Need to define CLIENT_SECRET environment variable")
 
-    AUTHORITY = "https://login.microsoftonline.com/common"  # For multi-tenant app, else put tenant name
+    AUTHORITY = "https://login.microsoftonline.com/5244b6c3-3240-4504-8ebf-d6ec97e7eec2"  # For multi-tenant app, else put tenant name
     # AUTHORITY = "https://login.microsoftonline.com/Enter_the_Tenant_Name_Here"
 
-    CLIENT_ID = "ENTER_CLIENT_ID_HERE"
+    CLIENT_ID = "fd752ed1-26eb-4200-8308-491c2dd61ee7"
 
     REDIRECT_PATH = "/getAToken"  # Used to form an absolute URL; must match to app's redirect_uri set in AAD
 
